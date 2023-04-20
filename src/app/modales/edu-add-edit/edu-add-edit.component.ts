36c4ea4aea06b7ce530b215e767e296ed4ac59ee
Paula <pacaviglia@gmail.com>
2023-04-20 19:38:16 -0300

    form group y metodos

--- src/app/modales/edu-add-edit/edu-add-edit.component.ts
+++ src/app/modales/edu-add-edit/edu-add-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edu-add-edit',
@@ -6,5 +8,99 @@ import { Component } from '@angular/core';
   styleUrls: ['./edu-add-edit.component.css']
 })
 export class EduAddEditComponent {
+
+  traerData: any;
+  editar: any;
+  addMode = true;
+  id: any;
+
+  public formAddEditEdu = new FormGroup({
+    id: new FormControl({ value: 0, disabled: true }),
+    nombre: new FormControl('', Validators.compose([Validators.required])),
+    logo: new FormControl('', Validators.compose([Validators.required])),
+    inicio: new FormControl(null, Validators.compose([Validators.required])),
+    fin: new FormControl(null, Validators.compose([Validators.required])),
+    descripcion: new FormControl('', Validators.compose([Validators.required])),
+  });
+
+  @ViewChild('content') editview!: ElementRef;
+
+  //Constructor
+  constructor(
+    private modalService: NgbModal,
+    private educacionService: EducacionService
+  ) { }
+
+  get Nombre() {
+    return this.formAddEditEdu.get('nombre');
+  }
+
+  get Logo() {
+    return this.formAddEditEdu.get('logo');
+  }
+
+  get Inicio() {
+    return this.formAddEditEdu.get('inicio');
+  }
+
+  get Fin() {
+    return this.formAddEditEdu.get('fin');
+  }
+
+  get Descripcion() {
+    return this.formAddEditEdu.get('descripcion');
+  }
+
+  onGet(id: any) {
+    this.id = this.educacionService.verEducacion(id);
+    if (id === undefined) {
+      this.addMode = true;
+      this.formAddEditEdu.reset();
+      console.log("paso 1");
+    } else {
+      this.addMode = false;
+      this.educacionService.verEducacion(id).subscribe((data) => {
+        this.traerData = data;
+        console.log("paso 2");
+        this.formAddEditEdu.setValue({
+          id: this.traerData.id,
+       nombre: this.traerData.nombre,
+       inicio: this.traerData.inicio,
+       fin: this.traerData.fin,
+       descripcion: this.traerData.descripcion,
+       logo: ''
+        });
+      }
+      )
+    };
+    this.abrir();
+  }
+
+  abrir() {
+    this.modalService.open(this.editview)
+      .result.then((result) => { }, (reason) => { }
+      );
+  }
+
+ onSave(event: Event) {
+    //evita que se ejecute el submit
+    event.preventDefault;
+    if (this.formAddEditEdu.valid) {
+      this.educacionService.updateEducacion(this.formAddEditEdu.getRawValue()).subscribe(data => {
+        this.editar = data;
+      })
+      if (this.addMode) {
+        alert("Estudio agregada correctamente")
+        window.location.reload();
+      }
+      if (!this.addMode) {
+        alert("Estudio editada correctamente")
+        window.location.reload();
+      }
+    } else {
+      alert("Error, intente nuevamente")
+      this.formAddEditEdu.markAllAsTouched();
+    }
+  }
 
 }
