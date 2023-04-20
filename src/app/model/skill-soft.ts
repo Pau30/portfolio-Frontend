b6b095e371c9a328f76a6cfa8c6e2eb76e6a4c96
Paula <pacaviglia@gmail.com>
2023-04-20 19:15:56 -0300

    skill soft model

---
+++ src/app/model/skill-soft.ts
@@ -0,0 +1,13 @@
+export class SkillSoft {
+  id:number;
+  nombre: string;
+  descripcion:string;
+  icon: string
+
+
+    constructor(nombre: string, descripcion:string, icon:string) {
+     this.nombre = nombre;
+     this.descripcion = descripcion;
+     this.icon= icon;
+}
+}
