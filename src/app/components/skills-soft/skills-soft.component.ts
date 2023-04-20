a60b63f6d695c2a35589c50df07e7a595b0fec23
Paula <pacaviglia@gmail.com>
2023-04-20 10:14:27 -0300

    skills Soft creado

---
+++ src/app/components/skills-soft/skills-soft.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-soft',
+  templateUrl: './skills-soft.component.html',
+  styleUrls: ['./skills-soft.component.css']
+})
+export class SkillsSoftComponent {
+
+}
