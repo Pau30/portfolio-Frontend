d171a79c6d87f7c9af934b90dff0dec3171bdc71
Paula <pacaviglia@gmail.com>
2023-04-20 16:10:25 -0300

    variables y constructor agregados

--- src/app/model/skill-hard.ts
+++ src/app/model/skill-hard.ts
@@ -1,4 +1,13 @@
 export class SkillHard {
+  id!:number;
+  nombre: string;
+  porcentaje:number;
+  tipo:string;
 
+
+    constructor( nombre: string, porcentaje:number, tipo:string) {
+      this.nombre = nombre;
+     this.porcentaje = porcentaje;
+     this.tipo = tipo;
     }
-
+  }
