d4a79f3e325f1f48a20c15e37e7273f103250a09
Paula <pacaviglia@gmail.com>
2023-04-20 16:21:20 -0300

    contructor + metodo cargar y delete

--- src/app/components/skills-hard/skills-hard.component.ts
+++ src/app/components/skills-hard/skills-hard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SkillHard } from 'src/app/model/skill-hard';
+import { SkillHardService } from 'src/app/services/skill-hard.service';
+
 
 @Component({
   selector: 'app-skills-hard',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills-hard.component.css']
 })
 export class SkillsHardComponent {
+  skills: SkillHard[]=[];
+
+  constructor (private skillHardService: SkillHardService){}
+
+  ngOnInit(): void{
+    this.cargarSkill();}
+
+    cargarSkill(): void{
+      this.skillHardService.listSkills().subscribe(data =>{this.skills=data});
+     }
+
+
+     deleteSkill(id:number){
+      if(confirm('¿Estas seguro que queres eliminar esta skill?')){
+        this.skillHardService.eliminarSkills(id).subscribe(data =>{
+          this.cargarSkill();})
+      }
+     }
 
 }
