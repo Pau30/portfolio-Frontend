24b173c2e5fc6dde73bae17f0d565ef63e93a5d2
Paula <pacaviglia@gmail.com>
2023-04-20 10:16:21 -0300

    persona model creado

---
+++ src/app/model/persona.ts
@@ -0,0 +1,14 @@
+export class Persona {
+
+  id!: number;
+  nombre: string;
+  descripcion: string;
+  imagen: string;
+
+  constructor(nombre: string, descripcion: string, imagen: string) {
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.imagen = imagen;
+  }
+
+}
