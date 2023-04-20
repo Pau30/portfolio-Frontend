6bbda9a2313b49fc9aa5274e242899727aca4a73
Paula <pacaviglia@gmail.com>
2023-04-20 19:26:52 -0300

    edu modal creado

---
+++ src/app/modales/edu-add-edit/edu-add-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edu-add-edit',
+  templateUrl: './edu-add-edit.component.html',
+  styleUrls: ['./edu-add-edit.component.css']
+})
+export class EduAddEditComponent {
+
+}
