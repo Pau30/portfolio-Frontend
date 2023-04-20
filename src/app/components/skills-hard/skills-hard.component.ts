de5cabc68eb1ced0755d7a713eea0aa5638255a4
Paula <pacaviglia@gmail.com>
2023-04-20 10:13:46 -0300

    skills hard creado

---
+++ src/app/components/skills-hard/skills-hard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills-hard',
+  templateUrl: './skills-hard.component.html',
+  styleUrls: ['./skills-hard.component.css']
+})
+export class SkillsHardComponent {
+
+}
