3793079938d496aafc016d23de699f1f8c9eb86d
Paula <pacaviglia@gmail.com>
2023-04-20 19:47:32 -0300

    clases de usuario terminadas

---
+++ src/app/model/login-usuario.ts
@@ -0,0 +1,10 @@
+export class LoginUsuario {
+  public nombreUsuario: string;
+  public password: string;
+
+  constructor(nombreUsuario: string, password: string) {
+    this.nombreUsuario = nombreUsuario;
+    this.password = password;
+  }
+}
+
---
+++ src/app/model/nuevo-usuario.ts
@@ -0,0 +1,12 @@
+export class NuevoUsuario {
+  nombre: string;
+  nombreUsuario: string;
+  email:string;
+  password: string;
+  authorities: string[];
+
+  constructor(nombreUsuario: string, password: string) {
+    this.nombreUsuario = nombreUsuario;
+    this.password = password;
+  }
+}
