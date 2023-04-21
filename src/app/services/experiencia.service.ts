99a4c08bf58f5a0e6cd01a37677e6e025058e2ef
Paula <pacaviglia@gmail.com>
2023-04-21 11:45:24 -0300

    terminado

---
+++ src/app/services/experiencia.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienciaService {
+
+  url= 'http://localhost:8080/experiencia/';
+
+  constructor(private httpClient:HttpClient) { }
+
+  public listExperiencia(): Observable<Experiencia[]>{
+    return this.httpClient.get<Experiencia[]>(this.url + 'lista');
+  }
+
+  public verExperiencia(id:number): Observable<Experiencia>{
+    return this.httpClient.get<Experiencia>(this.url + `ver/${id}`);
+  }
+
+  public agregarExperiencia(experiencia: Experiencia): Observable<any>{
+    return this.httpClient.post<any>(this.url + 'crear', experiencia );
+  }
+
+  public eliminarExperiencia(id:number): Observable<any>{
+    return this.httpClient.delete<any>(this.url + `borrar/${id}`);
+  }
+
+  public updateExperiencia(experiencia:Experiencia): Observable<any>{
+    return this.httpClient.put<any>(this.url + 'editar', experiencia);
+  }
+}
