27da32fb9e43123b50dfadbbbc10b7e283a42065
Paula <pacaviglia@gmail.com>
2023-04-21 12:31:03 -0300

    modo loggedIn agregado y funcionando

--- src/app/components/aboutme/aboutme.component.ts
+++ src/app/components/aboutme/aboutme.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Persona } from 'src/app/model/persona';
 import { AboutmeService } from 'src/app/services/aboutme.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-aboutme',
@@ -9,11 +10,18 @@ import { AboutmeService } from 'src/app/services/aboutme.service';
 })
 export class AboutmeComponent {
   persona: Persona = new Persona("", "", "");
-
-  constructor(public aboutmeService: AboutmeService) { }
+  loggedIn = false;
+  constructor(private aboutmeService: AboutmeService, private tokenService:TokenService) { }
 
   ngOnInit(): void {
-    this.cargarPersona(1);}
+    this.cargarPersona(1);
+    if (this.tokenService.getToken()) {
+      console.log("Logeado");
+      this.loggedIn = true;
+    } else {
+      this.loggedIn=false;
+    }
+  }
 
 cargarPersona(id: number): void {
   this.aboutmeService.verPersona(id).subscribe(data => { this.persona = data });
