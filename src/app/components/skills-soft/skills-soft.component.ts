49204f27d85671776c26dd32dff3d946f7667ff5
Paula <pacaviglia@gmail.com>
2023-04-21 15:22:37 -0300

    soft skill con loggedin funcionando

--- src/app/components/skills-soft/skills-soft.component.ts
+++ src/app/components/skills-soft/skills-soft.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SkillSoftAddEditComponent } from 'src/app/modales/skill-soft-add-edit/skill-soft-add-edit.component';
+import { SkillSoft } from 'src/app/model/skill-soft';
+import { SkillSoftService } from 'src/app/services/skill-soft.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-skills-soft',
@@ -6,5 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills-soft.component.css']
 })
 export class SkillsSoftComponent {
+  skillSoft: SkillSoft[] = [];
+  loggedIn=false;
+  id: any;
 
+   //Permite tener acceso a las funciones del modal editar//
+  @ViewChild(SkillSoftAddEditComponent) editSoftView !: SkillSoftAddEditComponent
+
+
+  constructor(private skillSoftService: SkillSoftService, private tokenService:TokenService) { }
+
+  ngOnInit(): void {
+    this.cargarSkillSoft();
+    if (this.tokenService.getToken()) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn=false;
+    }
+    }
+
+  cargarSkillSoft(): void {
+    this.skillSoftService.listSkills().subscribe(data => { this.skillSoft = data });
+  }
+
+  addEditSkill(id:any){
+    this.editSoftView.onGet(id);
+    }
+
+  deleteSkill(id: number) {
+    if (confirm('¿Estas seguro que queres eliminar esta skill?')) {
+      this.skillSoftService.eliminarSkills(id).subscribe(data => {
+        this.cargarSkillSoft();
+      })
+    }
+  }
 }
