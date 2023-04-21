28f4795b2a97d9c65d14a2617520f0a893042fcb
Paula <pacaviglia@gmail.com>
2023-04-21 11:27:33 -0300

    clase terminada

---
+++ src/app/model/experiencia.ts
@@ -0,0 +1,16 @@
+export class Experiencia {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  logo: string;
+  inicio: number;
+  fin: number;
+
+  constructor(nombre: string, descripcion: string, logo: string, inicio:number, fin:number) {
+   this.nombre = nombre;
+   this.descripcion = descripcion;
+   this.logo = logo;
+   this.inicio = inicio;
+   this.fin = fin;
+}
+}
