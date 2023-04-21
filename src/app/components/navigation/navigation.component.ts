404cde67a8d020c36b7c731349e68270efb16290
Paula <pacaviglia@gmail.com>
2023-04-21 12:23:00 -0300

    botnes de login y metodos agregados

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
+
+
 export class NavigationComponent {
+
+  loggedIn = false;
+
+  constructor(private tokenService: TokenService) { }
+
+  ngOnInit(): void {
+
+    if (this.tokenService.getToken()) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+
+  onLogout(): void {
+    this.tokenService.logOut();
+    window.location.reload();
+  }
 
 }
