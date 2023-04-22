1b85785ef189cc1cdde3d1298bac26378eb2e048
Paula <pacaviglia@gmail.com>
2023-04-22 12:02:00 -0300

    terminado

--- src/app/components/experiencia/experiencia.component.ts
+++ src/app/components/experiencia/experiencia.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Experiencia } from 'src/app/model/experiencia';
+import { ExperienciaService } from 'src/app/services/experiencia.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-experiencia',
@@ -7,4 +10,40 @@ import { Component } from '@angular/core';
 })
 export class ExperienciaComponent {
 
+//traer datos distintos datos del array
+experiencias: Experiencia[]=[];
+selectedItem=1;
+loggedIn=false;
+id:any;
+
+constructor (private experieciaService:ExperienciaService, private tokenService:TokenService) {}
+
+ ngOnInit(): void{
+
+ this.cargarExperiencia();
+ if (this.tokenService.getToken()) {
+  this.loggedIn = true;
+} else {
+  this.loggedIn=false;
+}
+
+}
+cargarExperiencia(): void{
+  this.experieciaService.listExperiencia().subscribe(data =>{this.experiencias=data});
+ }
+
+ seleccionado(id){
+  this.selectedItem = id;
+ }
+
+ addEditExperiecia(id:any){
+
+ }
+
+  deleteExperiencia(id:number){
+   if(confirm('¿Estas seguro que queres eliminar este estudio?')){
+     this.experieciaService.eliminarExperiencia(id).subscribe(data =>{
+       this.cargarExperiencia();})
+   }
+  }
 }
