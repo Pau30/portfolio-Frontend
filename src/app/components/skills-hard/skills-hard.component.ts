1110c1eb93b858398363cb6a5307f29f65314591
Paula <pacaviglia@gmail.com>
2023-04-23 11:38:18 -0300

    efecto fadein funcionando

--- src/app/components/skills-hard/skills-hard.component.ts
+++ src/app/components/skills-hard/skills-hard.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 import { SkillAddEditComponent } from 'src/app/modales/skill-add-edit/skill-add-edit.component';
 import { SkillHard } from 'src/app/model/skill-hard';
 import { SkillHardService } from 'src/app/services/skill-hard.service';
 import { TokenService } from 'src/app/services/token.service';
+
 
 
 @Component({
@@ -10,36 +11,68 @@ import { TokenService } from 'src/app/services/token.service';
   templateUrl: './skills-hard.component.html',
   styleUrls: ['./skills-hard.component.css']
 })
-export class SkillsHardComponent {
-  skills: SkillHard[]=[];
-  loggedIn=false;
+
+export class SkillsHardComponent implements OnInit, AfterViewInit {
+  skills: SkillHard[] = [];
+  loggedIn = false;
   id: any;
 
-  constructor (private skillHardService: SkillHardService, private tokenService:TokenService){}
-//Permite tener acceso a las funciones del modal editar//
-@ViewChild(SkillAddEditComponent) editView !:SkillAddEditComponent
+  constructor(private skillHardService: SkillHardService, private tokenService: TokenService, private el: ElementRef) { }
+  //Permite tener acceso a las funciones del modal editar//
+  @ViewChild(SkillAddEditComponent) editView !: SkillAddEditComponent
 
-  ngOnInit(): void{
-    this.cargarSkill();}
 
-    cargarSkill(): void{
-      this.skillHardService.listSkills().subscribe(data =>{this.skills=data});
-      if (this.tokenService.getToken()) {
-        this.loggedIn = true;
-      } else {
-        this.loggedIn=false;
-      }
-     }
+  ngOnInit(): void {
+    this.cargarSkill();
+  }
 
-     addEditSkill(id:any){
-      this.editView.onGet(id);
-      }
+  cargarSkill(): void {
+    this.skillHardService.listSkills().subscribe(data => { this.skills = data });
+    if (this.tokenService.getToken()) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
 
-     deleteSkill(id:number){
-      if(confirm('¿Estas seguro que queres eliminar esta skill?')){
-        this.skillHardService.eliminarSkills(id).subscribe(data =>{
-          this.cargarSkill();})
-      }
-     }
+  addEditSkill(id: any) {
+    this.editView.onGet(id);
+  }
+
+  deleteSkill(id: number) {
+    if (confirm('¿Estas seguro que queres eliminar esta skill?')) {
+      this.skillHardService.eliminarSkills(id).subscribe(data => {
+        this.cargarSkill();
+      })
+    }
+  }
+
+  ngAfterViewInit() {
+    const threshold = 0.5;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('fadeIn')
+          }
+          else {
+            if (entry.intersectionRatio > 0)
+              entry.target.classList.remove('fadeIn');
+          }
+        });
+      },
+      { threshold }
+    );
+    observer.observe(this.hardskill.nativeElement);
+    observer.observe(this.hardskill1.nativeElement);
+    observer.observe(this.hardskill2.nativeElement);
+    observer.observe(this.hardskill3.nativeElement);
+  
+
+  }
+  @ViewChild('hardskill') hardskill!: ElementRef;
+  @ViewChild('hardskill1') hardskill1!: ElementRef;
+  @ViewChild('hardskill2') hardskill2!: ElementRef;
+  @ViewChild('hardskill3') hardskill3!: ElementRef;
 
 }
