ab4d9d34d7f41e2ef4b7ffd63b349d958452aca7
Paula <pacaviglia@gmail.com>
2023-04-27 15:21:38 -0300

    terminado

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { NuevoUsuario } from '../model/nuevo-usuario';
 import { Observable } from 'rxjs';
 import { LoginUsuario } from '../model/login-usuario';
 import { JwtDto } from '../model/jwt-dto';
@@ -14,9 +13,6 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any> {
-    return this.http.post<any>(this.url + 'nuevo', nuevoUsuario);
-  }
 
   public login(loginUsuario: LoginUsuario): Observable<JwtDto> {
     return this.http.post<JwtDto>(this.url + 'login', loginUsuario);
