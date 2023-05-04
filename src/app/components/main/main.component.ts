23a322dabb41ef57b9381cee807ecc2975d105e7
Paula <pacaviglia@gmail.com>
2023-05-04 16:41:31 -0300

    fondo y agregado de estilos

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, HostListener, Inject, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
+
+
+
 export class MainComponent {
 
+  windowScrolled: boolean;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+      if (document.documentElement.scrollTop || document.body.scrollTop) {
+          this.windowScrolled = true;
+      }
+     else {
+          this.windowScrolled = false;
+      }
+  }
+  scrollToTop(): void {
+    // scroll to the top of the body
+    return this.document.body.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'start'
+    });
+  }
 }
