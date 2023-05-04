0d4166caadaa25a9ee2a67aa8c6849277b23c2e1
Paula <pacaviglia@gmail.com>
2023-05-04 16:41:58 -0300

    errorpage route agregada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
 
 const routes: Routes = [
- {path: '', component:  MainComponent,}
+ {path: '', component:  MainComponent},
+ {path: '**', component:ErrorPageComponent}
 ];
 
 @NgModule({
