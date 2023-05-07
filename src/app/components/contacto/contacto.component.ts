9f553119ee867367b597e850d75e146396f340f8
Paula <pacaviglia@gmail.com>
2023-05-06 21:57:49 -0300

    contacto componente y modal edicion funcionando

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ContactoAddEditComponent } from 'src/app/modales/contacto-add-edit/contacto-add-edit.component';
+import { Contacto } from 'src/app/model/contacto';
+import { ContactoService } from 'src/app/services/contacto.service';
+import { TokenService } from 'src/app/services/token.service';
 
 @Component({
   selector: 'app-contacto',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
+  contacto: Contacto = new Contacto("", "");
+  id: any;
+  loggedIn = false;
+
+  //Permite tener acceso a las funciones del modal editar
+  @ViewChild(ContactoAddEditComponent) editView !: ContactoAddEditComponent
+
+
+  constructor(private contactoService: ContactoService, private tokenService: TokenService) { }
+  //Lo que se carga cuando se carga la pagina
+  ngOnInit(): void {
+    this.cargarContacto(1);
+    //Booleano que oculta los botones de edicion, etc si el usuario no esta loggedIn
+    if (this.tokenService.getToken()) {
+      console.log("Logeado");
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
+//Metodo para cargar los datos de contacto
+  cargarContacto(id: number): void {
+    this.contactoService.verContacto(id).subscribe(data => { this.contacto = data });
+  }
+
+  //Metodo para editar contacto
+  editContacto(id: any) {
+    this.editView.onGet(id);
+  }
 
 }
---
+++ src/app/modales/contacto-add-edit/contacto-add-edit.component.ts
@@ -0,0 +1,84 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ContactoService } from 'src/app/services/contacto.service';
+
+@Component({
+  selector: 'app-contacto-add-edit',
+  templateUrl: './contacto-add-edit.component.html',
+  styleUrls: ['./contacto-add-edit.component.css']
+})
+export class ContactoAddEditComponent {
+  traerData: any;
+  editar: any;
+  addMode = true;
+  id: any;
+
+
+
+  constructor(
+    private modalService: NgbModal, private contactoService: ContactoService) { }
+
+  //Da acceso al mismo elemento
+  @ViewChild('content') editview!: ElementRef;
+
+
+  ngOnInit() {
+   }
+
+ //Armado del Formgroup
+ formEditContacto = new FormGroup({
+  id: new FormControl({ value: 0, disabled: true }),
+  email: new FormControl('', Validators.compose([Validators.required, Validators.email])),
+  mensaje: new FormControl('', Validators.compose([Validators.required])),
+
+});
+
+
+  //Metodos para tomar datos
+  get Email() {
+    return this.formEditContacto.get('email');
+  }
+
+  get Mensaje() {
+    return this.formEditContacto.get('mensaje');
+  }
+
+  //Metodo para traer datos al modal
+  onGet(id: any) {
+    this.id = this.contactoService.verContacto(id);
+    this.contactoService.verContacto(id).subscribe((data) => {
+      this.traerData = data;
+      this.formEditContacto.setValue({
+        id: this.traerData.id,
+        email: this.traerData.email,
+        mensaje: this.traerData.mensaje,
+
+      });
+    }
+    );
+    this.abrir();
+  }
+
+  //Metodo para abrir el modal
+  abrir() {
+    this.modalService.open(this.editview)
+      .result.then((result) => { }, (reason) => { }
+      );
+  }
+
+  //Metodo para guardar los cambios o avisar si hay error
+  onSave(event: Event) {
+    event.preventDefault;
+    if (this.formEditContacto.valid) {
+      this.contactoService.updateContacto(this.formEditContacto.getRawValue()).subscribe(data => {
+        this.editar = data;
+        alert("Contacto editada correctamente")
+        window.location.reload();
+      })
+    } else {
+      alert("Error, intente nuevamente")
+      this.formEditContacto.markAllAsTouched();
+    }
+}
+}
