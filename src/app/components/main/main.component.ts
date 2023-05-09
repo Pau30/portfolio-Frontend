30d3f4ad3fdcd9587127f7393d80d7f67875d4ab
Pau30 <116812169+Pau30@users.noreply.github.com>
2023-05-09 00:13:40 -0300

    welcome terminado

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -11,24 +11,4 @@ import { Component, ElementRef, HostListener, Inject, ViewChild } from '@angular
 
 export class MainComponent {
 
-  windowScrolled: boolean;
-
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-  @HostListener("window:scroll", [])
-  onWindowScroll() {
-      if (document.documentElement.scrollTop || document.body.scrollTop) {
-          this.windowScrolled = true;
-      }
-     else {
-          this.windowScrolled = false;
-      }
-  }
-  scrollToTop(): void {
-    // scroll to the top of the body
-    return this.document.body.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-      inline: 'start'
-    });
-  }
 }
